55995fe080ec824d828fc738af0f4dcd7035bab5
Renée Hendricksen <renee@nird.us>
2014-05-15 20:57:43 -0400

    bring back our many to many relationship for students and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ActiveRecord::Base
   has_many :registrations
-  #has_many :students, through: :registrations
+  has_many :students, through: :registrations
   belongs_to :student
 
   scope :rails, -> { where('title LIKE ?', '%Rails%')}
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
   has_many :registrations
-  has_many :courses#, through: :registrations
+  has_many :courses, through: :registrations
 
   accepts_nested_attributes_for :courses
 
